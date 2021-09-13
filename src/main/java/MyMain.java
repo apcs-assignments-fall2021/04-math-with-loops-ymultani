8e260a89ebe28fce4e30770ddf4094e206c59124
yuvro <23ymultani@athenian.org>
2021-09-12 21:40:48 -0700

    need help in class

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,22 +4,40 @@ public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
+        int base_num = 1;
+        int factorial_num = 1;
+        while (base_num <= x) {
+            factorial_num = factorial_num * base_num;
+            base_num++;
 
+        }
+        return factorial_num;
+
+    }
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double e_compare = 0;
+        int e_tester = 1;
+        double e_val = Math.E;
+        while (e_val - e_compare >= 0.00000000001){
+            e_compare = (1/factorial(e_tester));
+            e_tester++;
+        }
+        return Math.E;
+
+
     }
 
-    // Calculates the square root iteratively, using the Babylonian method
+    // Could not figure out how to do it
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double n = 41;
+        double m = 0;
+        while(Math.abs(x-m) > 0.0000001){
+
+        }
+
     }
     
     
